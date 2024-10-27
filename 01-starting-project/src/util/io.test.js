2394493a6018dd0b11979dda6cbb0911c8b171c2
CarlesRa <jcramos8282@gmail.com>
2024-10-27 11:32:55 +0100

    mocks

--- 01-starting-project/src/util/io.test.js
+++ 01-starting-project/src/util/io.test.js
@@ -1,9 +1,26 @@
-import { it, expect } from 'vitest';
+import { it, expect, vi } from 'vitest';
+import { promises as fs } from 'fs';
+
 import writeData from './io';
+
+vi.mock('fs');
+vi.mock('path', () => {
+  return {
+    default: {
+      join: (...args) => {
+        return args[args.length - 1];
+      }
+    }
+  }
+});
 
 it('should execut the writefile method', () => {
   const testData = 'Test';
   const testFileName = 'test.txt';
 
-  return expect(writeData(testData, testFileName)).resolves.toBeUndefined();
+  writeData(testData, testFileName)
+
+  //return expect(writeData(testData, testFileName)).resolves.toBeUndefined();
+  // expect(fs.writeFile).toBeCalled();
+  expect(fs.writeFile).toBeCalledWith(testFileName, testData);
 })
